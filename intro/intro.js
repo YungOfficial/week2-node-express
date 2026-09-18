51d3fb4eb19b981737784beaba0ee66ef6e75345
YungOfficial <rokeebabiodun32@gmail.com>
2026-09-18 06:10:43 +0100

    week 2 node express

---
+++ intro/intro.js
@@ -0,0 +1,8 @@
+// intro.js
+let name = "Ismail Rokeeb";
+let age = 22;
+let favoriteLanguage = "JavaScript";
+
+console.log("Name:", name);
+console.log("Age:", age);
+console.log("Favorite Programming Language:", favoriteLanguage);
